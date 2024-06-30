160ff00d1c2cb7c8d1b7ebd0a57f5306267ef678
Túlius <tulius_sr@outlook.com>
2024-06-29 22:36:12 -0300

    Adicionando base do projeto

---
+++ forumHub/src/test/java/com/tulius/forumHub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tulius.forumHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
