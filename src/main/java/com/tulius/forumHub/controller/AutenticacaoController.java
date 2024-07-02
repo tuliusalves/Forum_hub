1b3d847f00ec0eaa9ba071961ed6d8e656564ee4
Túlius <tulius_sr@outlook.com>
2024-07-02 12:34:31 -0300

    Adicionando Controller e Service de autenticacao. Adicionando pacote Security

---
+++ src/main/java/com/tulius/forumHub/controller/AutenticacaoController.java
@@ -0,0 +1,46 @@
+package com.tulius.forumHub.controller;
+
+import com.tulius.forumHub.dto.autenticacao.DadosAutenticacao;
+import com.tulius.forumHub.dto.status.DadosErros;
+import com.tulius.forumHub.models.Usuario;
+import com.tulius.forumHub.security.DadosTokenJWT;
+import com.tulius.forumHub.security.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Tag(name = "Publico Controller", description = "Endpoints Públicos")
+    @Operation(summary = "Faça login", description = "Faça login na API e gere um Token JWT e insira em Authorize para ter acesso aos endpoints restritos")
+    @PostMapping
+    public ResponseEntity<?> efetuarLogin(@RequestBody @Valid DadosAutenticacao dadosAutenticacao) {
+
+        Authentication Authenticationtoken = new UsernamePasswordAuthenticationToken(dadosAutenticacao.email(), dadosAutenticacao.senha());
+        Authentication authentication = authenticationManager.authenticate(Authenticationtoken);
+        String tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        if(tokenJWT == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new DadosErros("Erro ao gerar token JWT!"));
+        }
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+    }
+}
---
+++ src/main/java/com/tulius/forumHub/security/SecurityFilter.java
@@ -0,0 +1,99 @@
+package com.tulius.forumHub.security;
+
+import com.tulius.forumHub.repositories.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();
+        if (request.getMethod().equalsIgnoreCase(HttpMethod.POST.toString()) &&
+                (requestURI.equalsIgnoreCase("/auth") ||
+                        requestURI.equalsIgnoreCase("/usuarios")
+                ) ||
+                requestURI.toLowerCase().startsWith("/swagger-ui") ||
+                requestURI.toLowerCase().startsWith(("/v3/api-docs"))) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String tokenJWT = recuperarToken(request);
+        if (!isTokenFornecido(tokenJWT, response)) {
+            return;
+        }
+
+        String subject = tokenService.getSubject(tokenJWT);
+        if (!isSubjectExistente(subject, response)) {
+            return;
+        }
+
+        UserDetails usuarioDetails = usuarioRepository.findByEmail(subject);
+        if(!verificaUsuarioExiste(usuarioDetails, response)){
+            return;
+        }
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(usuarioDetails, null, usuarioDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean verificaUsuarioExiste(UserDetails userDetails, HttpServletResponse response) throws IOException{
+        if(userDetails == null){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json; charset=UTF-8");
+            response.getWriter().write("{\"error\": \"Este token não é mais válido, gere-o novamente!\"}");
+            return false;
+        }
+        return true;
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
+
+    private boolean isTokenFornecido(String tokenJWT, HttpServletResponse response) throws IOException {
+        if (tokenJWT == null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json; charset=UTF-8");
+            response.getWriter().write("{\"error\": \"Token JWT não fornecido!\"}");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isSubjectExistente(String subject, HttpServletResponse response) throws IOException {
+        if (subject == null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json; charset=UTF-8");
+            response.getWriter().write("{\"error\": \"Token JWT inválido ou expirado!\"}");
+            return false;
+        }
+        return true;
+    }
+}
+
